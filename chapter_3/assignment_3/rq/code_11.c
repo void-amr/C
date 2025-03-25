2fa440ac43bb5996564d2c57388408b6d6386faf
void-amr <aryanranjane9@gmail.com>
2025-03-25 23:51:52 +0530

    25/3/25 11:50 PM

---
+++ chapter_3/assignment_3/rq/code_11.c
@@ -0,0 +1,18 @@
+/**
+ * 	@Author	:- Aryan Mahesh Ranjane	<ranjanearyan82@gmail.com> 
+ * 	@Date	:- 25/3/25 
+ */ 
+ 
+ // Header Files
+ #include <stdio.h> 
+ 
+ int main(void) 
+ { 
+	 // Entry point function. 
+
+	 int c1 = 'E', c2 = 5, c3 = '?';
+
+	 printf("Value of operation:- %d", (int)(69.0/5.0) * 63);
+	
+	 return 0; 
+ } 
