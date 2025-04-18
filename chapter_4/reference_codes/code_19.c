3366dc5a35e14b9e8631b516b24f52c792477cbf
void-amr <aryanranjane9@gmail.com>
2025-04-18 13:02:40 +0000

    18/4/25 6:33 PM

---
+++ chapter_4/reference_codes/code_19.c
@@ -0,0 +1,20 @@
+/**
+ * 	@Author	:- Aryan Mahesh Ranjane	<ranjanearyan82@gmail.com> 
+ * 	@Date	:- 18/4/25 
+ */ 
+ 
+ // Header Files 
+ #include <stdio.h>  
+
+ int main(void) 
+ {
+	 // Entry point function. 
+         
+	 int i = 12345; 
+	 float x = 345.678; 
+
+	 printf("%3d %5d %08d\n",i,i,i);
+	 printf("%3f %5f %8f\n",x,x,x);
+
+	 return 0; 
+ }
---
+++ chapter_4/reference_codes/code_20.c
@@ -0,0 +1,19 @@
+/**
+ * 	@Author	:- Aryan Mahesh Ranjane	<ranjanearyan82@gmail.com> 
+ * 	@Date	:- 18/4/25 
+ */ 
+
+ // Header Files 
+ #include <stdio.h>
+ 
+ int main(void) 
+ {
+	 // Entry point function.
+	 
+	 float f; 
+
+	 scanf("%G",&f);
+	 printf("\n%g\n",f);
+
+	 return 0; 
+ }
