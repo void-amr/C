a5cd4e9578076cd908b85091570bb23ed48a1f70
void-amr <aryanranjane9@gmail.com>
2025-05-07 22:20:36 +0530

    7/5/25 10:20 PM

---
+++ chapter_4/problems/code_26.c
@@ -0,0 +1,19 @@
+/**
+ *  @Author :- Aryan Mahesh Ranjane <ranjanearyan82@gmail.com>
+ *  @Date   :- 7/5/25
+ */
+
+ // Header Files 
+ #include <stdio.h> 
+
+ int main(void)
+ {
+    // Entry point function..
+    
+    char text[80];
+
+    scanf("%s", text);
+    printf("%-8.8s",text);
+
+    return 0; 
+ }
