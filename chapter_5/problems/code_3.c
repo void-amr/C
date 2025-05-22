f5bf52aea5c945dc63fcb5e1116c892374af5c5c
void-amr <aryanranjane9@gmail.com>
2025-05-23 00:07:37 +0530

    23/5/25

--- chapter_5/problems/code_3.c
+++ chapter_5/problems/code_3.c
@@ -19,8 +19,27 @@
     printf("Enter the value of radius = "); 
     scanf("%f", &radius); 
 
-    /**/
-    printf("")
+    /*Calculating area of circle
+    
+        Radius -> line that connects edge to the center of the circle
+                  this line is of same length (l) from every edge of circle to center.
+        Diameter -> 2 radius's ie -> 2 edge connected together 
+        
+        R1---------c--------R2
+
+        Area -> PI * r*r --> 
+        PI -> Total length of circle from outter part and it's ratio with diameter
+        r*r -> is basically diameter
+
+        So we multiply the ratio of circle -> Circumference:Diameter -> which is universal for all circle -> 3.14159
+        with diameter -> twice the radius 
+    */
+
+    // applying formula. 
+    area = PI * radius * radius;
+    
+    // Printing the output. 
+    printf("Area of circle = %f", area);
     return 0;
  }
 
